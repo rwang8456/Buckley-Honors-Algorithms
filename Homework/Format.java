6b3a966a1211b8aa6e900d55028cba51d737d8de
rwang8456 <42977621+rwang8456@users.noreply.github.com>
2018-09-13 10:28:54 -0700

    Update Format.java

--- Homework/Format.java
+++ Homework/Format.java
@@ -5,10 +5,8 @@ public class Format {
 	public static void main(String args[]) throws FileNotFoundException {
 		File fromFile = new File("src/Format.java");
 		File toFile = new File("src/Formatted.java");
-		
 		Scanner input = new Scanner(fromFile);
 		PrintWriter output = new PrintWriter(toFile);
-		
 		while(input.hasNextLine()) {
 			String line = input.nextLine();
 			int tabCount = 0;
@@ -19,18 +17,20 @@ public class Format {
 				}
 				if(line.charAt(line.length() - x) == '{') {
 					for(int i = 0; i < line.length(); i++) {
-						if(line.charAt(i) == '\t') tabCount++;
+						if(line.charAt(i) == '\t') 
+							tabCount++;
 					}
 					output.println(line.substring(0, line.length() - x));
 					for(int i = 0; i < tabCount; i++)
 						output.print("\t");
 					output.println("{");
 				}
-				else output.println(line);
+				else 
+					output.println(line);
 			}
-			else output.println();
+			else 
+				output.println(line);
 		}
-		
 		input.close();
 		output.close();
 	}
