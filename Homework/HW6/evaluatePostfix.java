12838c0fb60e5388e16acbd51e686043d57947fe
rwang8456 <42977621+rwang8456@users.noreply.github.com>
2018-09-24 10:02:44 -0700

    Update evaluatePostfix.java

--- Homework/HW6/evaluatePostfix.java
+++ Homework/HW6/evaluatePostfix.java
@@ -5,42 +5,42 @@ public class evaluatePostfix
 	static int evaluatePostfix(String ex) 
 	{
 		int ans = 0;
-		Stack<Integer> s = new Stack<>();
+		Stack<Integer> st = new Stack<>();
 		for (int i = 0; i < ex.length(); i++) 
 		{
 			char ch = ex.charAt(i);
 			if ('1' <= ch && '9' >= ch) 
 			{
-				s.push(Integer.parseInt("" + ch));
+				st.push(Integer.parseInt("" + ch));
 			} 
 			else if (ch == '+') 
 			{
 				int b = s.pop();
 				int a = s.pop();
-				s.push(a + b);
+				st.push(a + b);
 			} 
 			else if (ch == '-') 
 			{
 				int b = s.pop();
 				int a = s.pop();
-				s.push(a - b);
+				st.push(a - b);
 			} 
 			else if (ch == '*') 
 			{
 				int b = s.pop();
 				int a = s.pop();
-				s.push(a * b);
+				st.push(a * b);
 			} 
 			else if (ch == '/') 
 			{
 				int b = s.pop();
 				int a = s.pop();
-				s.push(a / b);
+				st.push(a / b);
 			}
 			
 		}
 
-		return s.pop();
+		return st.pop();
 	}
 
 }
